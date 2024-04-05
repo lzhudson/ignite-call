2bf7105b7877c57a1d9f0d535edbe2a66b7e69dc
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-05 14:21:25 -0300

    feat: implement sign in with google

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -2,8 +2,11 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { Header, Container } from '../styles'
 import { ArrowRight } from 'phosphor-react'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function Register() {
+  const session = useSession()
+
   return (
     <Container>
       <Header>
@@ -17,11 +20,18 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
         </ConnectItem>
+
+        <Text>{JSON.stringify(session.data)}</Text>
+
         <Button type="submit">
           Próximo passo
           <ArrowRight />
