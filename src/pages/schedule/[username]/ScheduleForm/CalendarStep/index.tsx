21e6912808047b53b8a7535f7c82505840c88b1b
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-18 10:07:19 -0300

    feat: implement selected date to show or hide TimePicker component

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -6,13 +6,16 @@ import {
   TimePickerList,
   TimePickerItem,
 } from './style'
+import { useState } from 'react'
 
 export function CalendarStep() {
-  const isDateSelected = true
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+  const isDateSelected = !!selectedDate
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar />
+      <Calendar onDateSelected={setSelectedDate} selectedDate={selectedDate} />
 
       {isDateSelected && (
         <TimePicker>
