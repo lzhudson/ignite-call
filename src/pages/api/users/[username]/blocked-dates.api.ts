f0a0c98814c383db764bd4952f8ebb04f84f4349
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-20 18:15:10 -0300

    feat: building query to get blockedDates

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -42,5 +42,28 @@ export default async function handle(
     )
   })
 
-  return res.json({ blockedWeekDays })
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+    SELECT  
+      EXTRACT(DAY FROM S.date) AS date,
+      COUNT(S.date) AS amount,
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS SIZE
+    FROM schedulings S
+
+    LEFT JOIN user_time_intervals UTI
+      ON UTI.week_day = WEEKDAY(DATE_ADD(S.DATE, INTERVAL 1 DAY))
+
+    WHERE S.user_id = ${user.id}
+      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+
+    GROUP BY EXTRACT(DAY FROM S.date),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+    HAVING amount >= size
+  `
+
+  console.log(blockedDatesRaw)
+
+  const blockedDates = blockedDatesRaw.map((item) => item.date)
+
+  return res.json({ blockedWeekDays, blockedDates })
 }
