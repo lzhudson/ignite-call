7a6c85aaeca32ad9552eb1994384d9a7002ebbcf
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-21 13:57:55 -0300

    feat: define seo tags

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,29 +4,36 @@ import { Container, Hero, Preview } from './styles'
 import previewImage from '../../assets/app-preview.png'
 import Image from 'next/image'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading as="h1" size="4xl">
-          Agendamento Descomplicado
-        </Heading>
-        <Text size="lg">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          height={400}
-          quality={100}
-          priority
-          alt="Calendário Simbolizando aplicação em funcionamento"
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title="Descomplie sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading as="h1" size="4xl">
+            Agendamento Descomplicado
+          </Heading>
+          <Text size="lg">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendário Simbolizando aplicação em funcionamento"
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
