95247386196ea7c33dd37ba17bfe18625258a345
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-22 08:50:20 -0300

    style: disabled next line in eslint

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -100,6 +100,7 @@ export default function TimeIntervals() {
 
   const intervals = watch('intervals')
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async function handleSetTimeIntervals(data: any) {
     const { intervals } = data as TimeIntervalsFormDataOutput
 
